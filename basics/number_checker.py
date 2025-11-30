a28b7d31e485e23e75269e691dd1bfbf6b1a5bcd
Hugo Iboa <iboazain@gmail.com>
2025-11-30 15:44:45 -0800

    Added number_checker.py to practice if/elif/else conditionals

---
+++ basics/number_checker.py
@@ -0,0 +1,9 @@
+number = input("Enter a number: ")
+num = int(number)
+
+if num > 0:
+    print(number + " is positive")
+elif num < 0:
+    print(number + " is negative")
+elif num == 0:
+    print(number + " is zero ")
