4459accaa4fcc1ab904b87cf13b5fdc4b9ce8ee1
Hugo Iboa <iboazain@gmail.com>
2025-12-04 15:34:35 -0800

    Added OOP Calculator class with basic arithmetic operations

---
+++ oop/calculator.py
@@ -0,0 +1,19 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            return "Error: Cannot divide by zero"
+        return a / b
+
+
+calc = Calculator()
+print(calc.add(5, 2))
